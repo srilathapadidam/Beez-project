8becf6af128dd24341e77002068d80e1191ba23c
srilathapadidam <srilathapadidam@gmail.com>
2024-02-21 09:03:05 +0530

    first commit

---
+++ beez project/Assets/icons.jsx
@@ -0,0 +1,12 @@
+
+
+
+import {ReactComponent as MessageIcon}  from "../Assets/message.svg";
+import {ReactComponent as NotificationIcon}  from "../Assets/notification.svg";
+
+
+
+
+export {
+   MessageIcon,NotificationIcon
+}
